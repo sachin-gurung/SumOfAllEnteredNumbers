93b812a661abedcdfcfb94fca0e67ce9eb25095d
Sachin Gurung <saching061@hotmail.com>
2019-11-13 21:18:29 -0800

    First Commit

--- SumOfAllEnteredNumbers/Program.cs
+++ SumOfAllEnteredNumbers/Program.cs
@@ -10,6 +10,28 @@ namespace SumOfAllEnteredNumbers
     {
         static void Main(string[] args)
         {
+                var sum = 0;
+                while (true)
+                {
+                    Console.Write("Enter a number (or 'ok' to exit): ");
+                    var input = Console.ReadLine();
+
+                    if (input.ToLower() == "ok")
+                        break;
+
+                    sum += int.Parse(input);
+                }
+            Console.WriteLine("Sum of all number is: " + sum);
+
         }
     }
 }
+/*
+Enter a number (or 'ok' to exit): 5
+Enter a number (or 'ok' to exit): 6
+Enter a number (or 'ok' to exit): 3
+Enter a number (or 'ok' to exit): 4
+Enter a number (or 'ok' to exit): ok
+Sum of all number is: 18
+Press any key to continue . . .
+*/
