a4270a755418ec3028b5dae5b70f3301e5dc27b4
Sachin Gurung <saching061@hotmail.com>
2019-11-13 21:21:24 -0800

    Question typed

--- SumOfAllEnteredNumbers/Program.cs
+++ SumOfAllEnteredNumbers/Program.cs
@@ -8,6 +8,8 @@ namespace SumOfAllEnteredNumbers
 {
     class Program
     {
+        //Write a program and continuously ask the user to enter a number or "ok" to exit. 
+        //Calculate the sum of all the previously entered numbers and display it on the console.
         static void Main(string[] args)
         {
                 var sum = 0;
